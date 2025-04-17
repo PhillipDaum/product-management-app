654ad8daf1f95735cac918f0c42756207052100f
PhillipDaum <philgdaum@gmail.com>
2025-04-17 13:13:43 -0600

    add remote host

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://countries-api-project-56no.onrender.com/',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
